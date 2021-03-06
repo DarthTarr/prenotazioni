46df6cbeafee39f3757233d6573d7eefe357425b
DarthTarr <patrik.maniu@gmail.com>
2021-03-06 12:26:25 +0100

    Add data to the database

--- prenota.php
+++ prenota.php
@@ -1,2 +1,22 @@
 <?php
- $sasa = 3;
+ ini_set('display_errors', 1);
+ ini_set('log_errors', 0);
+ $host = 'localhost';
+ $db = 'prenotazioni';
+ $user = 'root';
+ $pass = '';
+
+ $dsn = 'mysql:host='.$host.';dbname='.$db;
+
+ $pdo = new PDO($dsn, $user, $pass);
+ $data_scelta = '2021-03-06';
+ $fiscale = 'asdfghjklqwertyu';
+ $sql = "INSERT INTO prenotazioni VALUES(null, :fiscale, :data_scelta)";
+ $stmt = $pdo->prepare($sql);
+
+$stmt->execute(
+    [
+        'fiscale' => $fiscale,
+        'data_scelta' => $data_scelta
+    ]
+);
